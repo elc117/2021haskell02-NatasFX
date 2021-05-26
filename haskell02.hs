83b132d2af7d8212ca7913b542b20510cacc7db4
NatasFX <nataschmitt02@hotmail.com>
2021-05-26 15:24:33 -0300

    Finished

--- haskell02.hs
+++ haskell02.hs
@@ -1,2 +1,35 @@
 -- Prática 02 de Haskell
--- Nome: 
+-- Nome: Natã Schmitt
+
+tempFebre :: Float -> Bool
+tempFebre = (> 37.8)
+
+comFebre :: [Float] -> [Float]
+comFebre = filter tempFebre
+
+comFebre' :: [Float] -> [Float]
+comFebre' = filter (> 37.8)
+
+itemize :: [String] -> [String]
+itemize = map (\s -> "<li>"++ s ++"</li>")
+
+bigCircles :: Float -> [Float] -> [Float]
+bigCircles area = filter (\r -> r^2*pi > area)
+
+--tempFebre1 :: (String,Float) -> Bool 
+--tempFebre1 s = ((last s) > 37.8)
+
+quarentena :: [(String,Float)] -> [(String,Float)]
+quarentena = filter (tempFebre . snd)
+
+idadesEm :: [Int] -> Int -> [Int]
+idadesEm l a = map (a+) l
+
+isUpper :: Char -> Bool
+isUpper s = elem s ['A'..'Z']
+
+changeNames :: [String] -> [String]
+changeNames s = (map ("Super "++)) (filter (isUpper . head) s)
+
+onlyShorts :: [String] -> [String]
+onlyShorts = filter (\s -> length s < 5)
